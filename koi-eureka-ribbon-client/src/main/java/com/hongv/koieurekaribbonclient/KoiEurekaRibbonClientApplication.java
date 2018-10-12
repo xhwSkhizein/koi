ba7c5a71e41ff17057790179a60c2d97913074c8
hongwei <skhizein0706@gmail.com>
2018-10-12 15:51:19 +0800

    + ribbon

---
+++ koi-eureka-ribbon-client/src/main/java/com/hongv/koieurekaribbonclient/KoiEurekaRibbonClientApplication.java
@@ -0,0 +1,15 @@
+package com.hongv.koieurekaribbonclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hongweixu
+ */
+@SpringBootApplication
+public class KoiEurekaRibbonClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoiEurekaRibbonClientApplication.class, args);
+    }
+}
