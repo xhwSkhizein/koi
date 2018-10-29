f2b0eef58979fd845254ca5907417bbc7caa60a6
xuhongwei5 <xuhongwei5@jd.com>
2018-10-29 17:45:20 +0800

    rm eureka client pom

--- koi-api/src/main/java/com/hongv/koi/KoiApiApplication.java
+++ koi-api/src/main/java/com/hongv/koi/KoiApiApplication.java
@@ -1,10 +1,7 @@
 package com.hongv.koi;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -12,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @SpringBootApplication
 @Configuration
-@EnableEurekaClient
 public class KoiApiApplication {
 
     public static void main(String[] args) {
